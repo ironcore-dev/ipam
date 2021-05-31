90546c3e6a95133dff275a57f8c00f8de21c3743
Andrei Ivashchenko <aiivashchenko@users.noreply.github.com>
2021-05-31 19:27:05 +0300

    Remove default comments with todos

--- api/v1alpha1/subnet_webhook.go
+++ api/v1alpha1/subnet_webhook.go
@@ -38,9 +38,6 @@ func (r *Subnet) SetupWebhookWithManager(mgr ctrl.Manager) error {
 		Complete()
 }
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-
-// TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
 // +kubebuilder:webhook:path=/validate-machine-onmetal-de-v1alpha1-subnet,mutating=false,failurePolicy=fail,sideEffects=None,groups=machine.onmetal.de,resources=subnets,verbs=create;update,versions=v1alpha1,name=vsubnet.kb.io,admissionReviewVersions={v1,v1beta1}
 
 var _ webhook.Validator = &Subnet{}
@@ -48,8 +45,6 @@ var _ webhook.Validator = &Subnet{}
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
 func (r *Subnet) ValidateCreate() error {
 	subnetlog.Info("validate create", "name", r.Name)
-
-	// TODO(user): fill in your validation logic upon object creation.
 	return nil
 }
 
@@ -98,7 +93,5 @@ func (r *Subnet) ValidateUpdate(old runtime.Object) error {
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
 func (r *Subnet) ValidateDelete() error {
 	subnetlog.Info("validate delete", "name", r.Name)
-
-	// TODO(user): fill in your validation logic upon object deletion.
 	return nil
 }
